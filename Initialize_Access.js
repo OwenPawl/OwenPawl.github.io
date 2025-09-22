8c7f1cdaf262a36d114837b40521b893bf150059
OwenPawl <125260660+OwenPawl@users.noreply.github.com>
2025-09-22 11:15:53 -0700

    Update Initialize_Access.js

--- Initialize_Access.js
+++ Initialize_Access.js
@@ -1,1 +1,21 @@
-
+if (!localStorage.getItem("access_token") && !window.location.hash.includes("access_token")) {
+  window.open(`https://pike13.com/oauth/authorize?client_id=ixG6UsN5JaLPYFgwjhTAtmdg8UNU7IZYK2lOxTo3&response_type=token&redirect_uri=${window.location.href}`);
+  };
+if (window.location.hash.includes("access_token")) {
+  localStorage.setItem("access_token", window.location.hash.slice(14,54));
+};
+if (!localStorage.getItem("Staff_ID")) {
+  const requestOptions = {
+    method: "GET",
+    headers: {Authorization: `${localStorage.getItem("access_token")}`},
+    redirect: "follow"
+  };
+  
+  fetch("https://example.com/api/accounts", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      // Extract the id from the first account
+      localStorage.setItem("Staff_ID", result.accounts?.[0]?.id);
+    })
+    .catch(error => console.error("Error:", error));
+};
