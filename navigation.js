4110f2453017a3168fb7adc23d67905aa722138e
OwenPawl <125260660+OwenPawl@users.noreply.github.com>
2025-09-24 07:14:01 -0700

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,18 +1,15 @@
-function load(file) {
-    fetch(file)
-    .then(response => {
-      if (!response.ok) throw new Error("Page not found: " + file);
-      return response.text();
-    })
+function load(file, scriptFile) {
+  fetch(file)
+    .then(r => r.text())
     .then(html => {
       document.body.innerHTML = html;
-      if (file != "navigation.html") {
-        const homeBtn = document.createElement("button");
-        homeBtn.textContent = "Go Back";
-        homeBtn.addEventListener("click", () => load("navigation.html"));
-        document.body.prepend(homeBtn);
+
+      if (scriptFile) {
+        const script = document.createElement("script");
+        script.src = scriptFile;
+        script.defer = true;
+        document.body.appendChild(script);
       }
-    })
-    .catch(err => console.error(err));
-};
-load("navigation.html");
+    });
+}
+load("navigation.html","getdata.js");
