72d7e6e5e4a18c02c7c26e02f87aff812a4eb0e3
OwenPawl <125260660+OwenPawl@users.noreply.github.com>
2025-09-24 02:48:18 -0700

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -5,14 +5,14 @@ function dateChanged(date) {
   const requestOptions = {
     method: "POST",
     headers: {"Authorization": "Bearer kZEbOpElCispz8mFkeoTsVGVCvSP23mZG82G7eeN","Content-Type":"application/json"},
-    body: {"data":{"type":"queries","attributes":{"page":{"limit":500},"fields":["full_name","service_time","first_visit","state","service_location_name","person_id","visit_id"],"filter":["and",[["eq","service_date",[`${date}`]],["eq","instructor_names",[`${localStorage.getItem("staff_name")}`]]]],"sort":["service_time"]}}},
+    body: JSON.stringify({"data":{"type":"queries","attributes":{"page":{"limit":500},"fields":["full_name","service_time","first_visit","state","service_location_name","person_id","visit_id"],"filter":["and",[["eq","service_date",[`${date}`]],["eq","instructor_names",[`${localStorage.getItem("staff_name")}`]]]],"sort":["service_time"]}}}),
     redirect: "follow"
   };
   
   fetch("https://mcdonaldswimschool.pike13.com/desk/api/v3/reports/enrollments/queries", requestOptions)
     .then(response => response.json())
     .then(result => {
-      sessionStorage.setItem("events", result.data?.attributes?.rows?);
+      sessionStorage.setItem("events", JSON.stringify(result.data?.attributes?.rows  || []));
     })
     .catch(error => console.error("Error:", error));
 }
