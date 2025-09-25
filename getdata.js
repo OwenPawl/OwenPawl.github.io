3061e2220fd693518f7d821a6feaf9058f5e7582
OwenPawl <125260660+OwenPawl@users.noreply.github.com>
2025-09-24 23:30:39 -0700

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -45,7 +45,10 @@ async function getevents() {
   return result;
 };
 function dateChanged(date) {
-  getevents().then(result => sessionStorage.setItem("schedule",JSON.stringify(result))).catch(console.error);
+  getevents().then(result => 
+    sessionStorage.setItem("schedule",JSON.stringify(result));
+    window.dispatchEvent(new CustomEvent("scheduleUpdated", { detail: result }));
+  ).catch(console.error);
 };
 if (!document.getElementById("dateInput").value) {
   document.getElementById("dateInput").value=new Date().toISOString().split("T")[0];
