21a08b920f3ade6b2fd593f06fd967f2044e0167
OwenPawl <125260660+OwenPawl@users.noreply.github.com>
2025-10-02 04:07:28 -0700

    Update attendance.js

--- attendance.js
+++ attendance.js
@@ -48,7 +48,7 @@ document.getElementById("submit").addEventListener("click", (event) => {
   async function Attendance(){
     await Promise.allSettled(attendance.filter(visit=>visit.type=="Check In"?"complete":"noshow"!=visit.state&&visit.state!="registered").map(visit=>fetch(desk+`visits/${visit.vid}`,{body:JSON.stringify({"visit":{"state_event":"reset"}}),method:"PUT",headers: {"Authorization": `Bearer ${localStorage.getItem("access_token")}`,"Content-Type": "application/json"},redirect: "follow"})));
     await Promise.allSettled(attendance.filter(visit=>visit.type=="Check In"?"complete":"noshow"!=visit.state).map(visit=>fetch(desk+`visits/${visit.vid}`,{body:JSON.stringify({"visit":{"state_event":visit.type=="Check In"?"complete":"noshow"}}),method:"PUT",headers: {"Authorization": `Bearer ${localStorage.getItem("access_token")}`,"Content-Type": "application/json"},redirect: "follow"})));
-    await Promise.allSettled(attendance.filter(visit=>visit.type=="No Show"&&visit.state!="noshow").map(visit=>fetch(desk+`punches`,{body:JSON.stringify({"punch":{"visit_id":${visit.vid}}}),method:"POST",headers: {"Authorization": `Bearer ${localStorage.getItem("access_token")}`,"Content-Type": "application/json"},redirect: "follow"})));
+    await Promise.allSettled(attendance.filter(visit=>visit.type=="No Show"&&visit.state!="noshow").map(visit=>fetch(desk+`punches`,{body:JSON.stringify({"punch":{"visit_id":visit.vid}}),method:"POST",headers: {"Authorization": `Bearer ${localStorage.getItem("access_token")}`,"Content-Type": "application/json"},redirect: "follow"})));
   };
   Attendance();
 });
